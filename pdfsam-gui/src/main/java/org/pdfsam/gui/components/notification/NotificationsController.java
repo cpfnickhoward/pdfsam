a0e5cd2f265de6fef29c00909ec3e7883fc77892
Prem Dahal <pdahal@ffsb.local>
2025-07-07 10:44:55 -0400

    remove notifications to share/donate

--- pdfsam-gui/src/main/java/org/pdfsam/gui/components/notification/NotificationsController.java
+++ pdfsam-gui/src/main/java/org/pdfsam/gui/components/notification/NotificationsController.java
@@ -115,48 +115,63 @@ public class NotificationsController {
         }
     }
 
-    @EventListener
-    public void onTaskCompleted(TaskExecutionCompletedEvent e) {
-        long usages = service.getTotalUsages();
-        if ((usages % TIMES_BEFORE_DONATION_NOTICE) == 0 && app().persistentSettings()
-                .get(BooleanPersistentProperty.DONATION_NOTIFICATION)) {
-            if ((random.nextInt() % 2) == 0) {
-                addDonationNotification(usages);
-            } else {
-                addShareNotification(usages);
-            }
-        }
-    }
+    // @EventListener
+    // public void onTaskCompleted(TaskExecutionCompletedEvent e) {
+    // long usages = service.getTotalUsages();
+    // if ((usages % TIMES_BEFORE_DONATION_NOTICE) == 0 &&
+    // app().persistentSettings()
+    // .get(BooleanPersistentProperty.DONATION_NOTIFICATION)) {
+    // if ((random.nextInt() % 2) == 0) {
+    // addDonationNotification(usages);
+    // } else {
+    // addShareNotification(usages);
+    // }
+    // }
+    // }
 
-    private void addDonationNotification(long usages) {
-        var content = new VBox(
-                buildLabel(i18n().tr("You performed {0} tasks with PDFsam, did it help?", Long.toString(usages)),
-                        NotificationType.SUPPORT), UrlButton.styledUrlButton(i18n().tr("Give something back"),
-                appBrand.property(BrandableProperty.DONATE_URL), null));
-        content.getStyleClass().add("notification-container");
+    // private void addDonationNotification(long usages) {
+    // var content = new VBox(
+    // buildLabel(i18n().tr("You performed {0} tasks with PDFsam, did it help?",
+    // Long.toString(usages)),
+    // NotificationType.SUPPORT),
+    // UrlButton.styledUrlButton(i18n().tr("Give something back"),
+    // appBrand.property(BrandableProperty.DONATE_URL), null));
+    // content.getStyleClass().add("notification-container");
 
-        container.addStickyNotification(i18n().tr("PDFsam worked hard!"), content);
-    }
+    // // container.addStickyNotification(i18n().tr("PDFsam worked hard!"),
+    // content);
+    // }
 
-    private void addShareNotification(long usages) {
-        Region spacer = new Region();
-        HBox.setHgrow(spacer, Priority.ALWAYS);
-        var bskyButton = UrlButton.styledUrlButton(i18n().tr("Spread the word!"),
-                appBrand.property(BrandableProperty.BLUESKY_SHARE_URL), null);
-        var bskySvg = new SVGPath();
-        bskySvg.setContent(
-                "M12 10.8c-1.087-2.114-4.046-6.053-6.798-7.995C2.566.944 1.561 1.266.902 1.565C.139 1.908 0 3.08 0 3.768c0 .69.378 5.65.624 6.479c.815 2.736 3.713 3.66 6.383 3.364q.204-.03.415-.056q-.207.033-.415.056c-3.912.58-7.387 2.005-2.83 7.078c5.013 5.19 6.87-1.113 7.823-4.308c.953 3.195 2.05 9.271 7.733 4.308c4.267-4.308 1.172-6.498-2.74-7.078a9 9 0 0 1-.415-.056q.21.026.415.056c2.67.297 5.568-.628 6.383-3.364c.246-.828.624-5.79.624-6.478c0-.69-.139-1.861-.902-2.206c-.659-.298-1.664-.62-4.3 1.24C16.046 4.748 13.087 8.687 12 10.8");
-        bskySvg.getStyleClass().add("svg-icon");
-        bskyButton.setGraphic(bskySvg);
-        var content = new VBox(
-                buildLabel(i18n().tr("You performed {0} tasks with PDFsam, did it help?", Long.toString(usages)),
-                        NotificationType.SHARE), new HBox(3, spacer,
-                UrlButton.styledUrlButton(null, appBrand.property(BrandableProperty.FACEBOOK_SHARE_URL),
-                        UniconsLine.FACEBOOK), bskyButton));
-        content.getStyleClass().add("notification-container");
+    // private void addShareNotification(long usages) {
+    // Region spacer = new Region();
+    // HBox.setHgrow(spacer, Priority.ALWAYS);
+    // var bskyButton = UrlButton.styledUrlButton(i18n().tr("Spread the word!"),
+    // appBrand.property(BrandableProperty.BLUESKY_SHARE_URL), null);
+    // var bskySvg = new SVGPath();
+    // bskySvg.setContent(
+    // "M12 10.8c-1.087-2.114-4.046-6.053-6.798-7.995C2.566.944 1.561 1.266.902
+    // 1.565C.139 1.908 0 3.08 0 3.768c0 .69.378 5.65.624 6.479c.815 2.736 3.713
+    // 3.66 6.383 3.364q.204-.03.415-.056q-.207.033-.415.056c-3.912.58-7.387
+    // 2.005-2.83 7.078c5.013 5.19 6.87-1.113 7.823-4.308c.953 3.195 2.05 9.271
+    // 7.733 4.308c4.267-4.308 1.172-6.498-2.74-7.078a9 9 0 0
+    // 1-.415-.056q.21.026.415.056c2.67.297 5.568-.628
+    // 6.383-3.364c.246-.828.624-5.79.624-6.478c0-.69-.139-1.861-.902-2.206c-.659-.298-1.664-.62-4.3
+    // 1.24C16.046 4.748 13.087 8.687 12 10.8");
+    // bskySvg.getStyleClass().add("svg-icon");
+    // bskyButton.setGraphic(bskySvg);
+    // var content = new VBox(
+    // buildLabel(i18n().tr("You performed {0} tasks with PDFsam, did it help?",
+    // Long.toString(usages)),
+    // NotificationType.SHARE),
+    // new HBox(3, spacer,
+    // UrlButton.styledUrlButton(null,
+    // appBrand.property(BrandableProperty.FACEBOOK_SHARE_URL),
+    // UniconsLine.FACEBOOK),
+    // bskyButton));
+    // content.getStyleClass().add("notification-container");
 
-        container.addStickyNotification(i18n().tr("PDFsam worked hard!"), content);
-    }
+    // container.addStickyNotification(i18n().tr("PDFsam worked hard!"), content);
+    // }
 
     @EventListener
     public void onRemoveRequest(RemoveNotificationRequest event) {
